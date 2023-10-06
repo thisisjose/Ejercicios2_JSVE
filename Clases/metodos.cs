4eb5ea33db55ae1fbcdf3fdd4c171b3de44f5962
Jose Valenzuela <josecinuwu@gmail.com>
2023-10-05 23:46:43 -0700

    Agregar archivos de proyecto.

---
+++ Clases/metodos.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios2_JSVE.Clases
+{
+    internal class metodos
+    {
+        //public static int[] SumaDeArreglos()
+        //{
+        //    Console.WriteLine("Escribe el tamaño");
+        //    int tamaño = Convert.ToInt32(Console.ReadLine());
+        //    int[] arreglo1 = new int[tamaño];
+        //    int[] arreglo2 = new int[tamaño];
+        //    int[] resultado = new int[tamaño];
+
+        //    Console.WriteLine("Escribe un numero para el arreglo 1:");
+        //    for (int x = 0; x < tamaño; x++)
+        //    {
+        //        Console.WriteLine($"valor{x + 1}:");
+        //        arreglo1[x] = int.Parse(Console.ReadLine());
+        //    }
+        //    Console.WriteLine("Escribe un segundo numero para el arreglo 2:");
+        //    for (int x = 0; x < tamaño; x++)
+        //    {
+        //        Console.WriteLine($"valor{x + 1}:");
+        //        arreglo2[x] = int.Parse(Console.ReadLine());
+        //    }
+
+        //    for (int x = 0; x < tamaño; x++)
+        //    {
+        //        resultado[x] = arreglo1[x] + arreglo2[x];
+        //    }
+        //    return resultado;
+        //}
+
+        //N2
+        //public void ValorArreglo(int[] arreglo)
+        //{
+        //    foreach (int valor in arreglo)
+        //    {
+        //        Console.Write(valor + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        //public int[] BuscarPosicionElemento(int[] arreglo, int elementoBuscado)
+        //{
+        //    int contadorCoincidencia = 0;
+        //    for (int i = 0; i < arreglo.Length; i++)
+        //    {
+        //        if (arreglo[i] == elementoBuscado)
+        //        {
+        //            contadorCoincidencia++;
+        //        }
+        //    }
+
+        //    int[] posicionesCoincidencia = new int[contadorCoincidencia];
+        //    contadorCoincidencia = 0;
+
+        //    for (int i = 0; i < arreglo.Length; i++)
+        //    {
+        //        if (arreglo[i] == elementoBuscado)
+        //        {
+        //            posicionesCoincidencia[contadorCoincidencia] = i;
+        //            contadorCoincidencia++;
+        //        }
+        //    }
+
+        //    return posicionesCoincidencia;
+        //}
+
+        //N3
+        //public int[] ObtenerIDs(string nombreClase)
+        //{
+        //    Console.WriteLine($"Ingrese la cantidad de alumnos en la clase de {nombreClase}:");
+        //    int cantidadAlumnos = Convert.ToInt32(Console.ReadLine());
+
+        //    int[] idsClase = new int[cantidadAlumnos];
+
+        //    for (int i = 0; i < cantidadAlumnos; i++)
+        //    {
+        //        Console.Write($"Ingrese el ID del alumno {i + 1} en {nombreClase}: ");
+        //        idsClase[i] = Convert.ToInt32(Console.ReadLine());
+        //    }
+
+        //    return idsClase;
+        //}
+
+        //public int[] EncontrarAlumnosComunes(int[] arreglo1, int[] arreglo2)
+        //{
+        //    var alumnosComunes = arreglo1.Intersect(arreglo2).ToArray();
+        //    return alumnosComunes;
+        //}
+
+        //public void MostrarAlumnos(int[] ids, string mensaje)
+        //{
+        //    Console.WriteLine(mensaje);
+        //    foreach (int id in ids)
+        //    {
+        //        Console.WriteLine($"ID del alumno: {id}");
+        //    }
+        //}
+
+        //public int[] LlenarArreglo()
+        //{
+        //    Console.WriteLine("Ingrese el tamaño del arreglo:");
+        //    int tamaño = int.Parse(Console.ReadLine());
+
+        //    int[] arreglo = new int[tamaño];
+        //    int indice = 0;
+
+        //    while (indice < tamaño)
+        //    {
+        //        Console.Write($"Ingrese el valor {indice + 1}: ");
+        //        int valor = int.Parse(Console.ReadLine());
+
+        //        if (!ContieneValor(arreglo, valor, indice))
+        //        {
+        //            arreglo[indice] = valor;
+        //            indice++;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("El valor ya existe en el arreglo. Ingrese otro valor.");
+        //        }
+        //    }
+
+        //    return arreglo;
+        //}
+
+        //private bool ContieneValor(int[] arreglo, int valor, int limite)
+        //{
+        //    for (int i = 0; i < limite; i++)
+        //    {
+        //        if (arreglo[i] == valor)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        public void CalcularPromedioYContar(int[] notas)
+        {
+            int aprobadas = 0;
+            int desaprobadas = 0;
+            int sumaAprobadas = 0;
+            int sumaDesaprobadas = 0;
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                if (notas[i] >= 8 && notas[i] <= 20)
+                {
+                    aprobadas++;
+                    sumaAprobadas += notas[i];
+                }
+                else if (notas[i] >= 0 && notas[i] <= 5)
+                {
+                    desaprobadas++;
+                    sumaDesaprobadas += notas[i];
+                }
+            }
+
+            double promedioAprobadas = aprobadas > 0 ? (double)sumaAprobadas / aprobadas : 0;
+            double promedioDesaprobadas = desaprobadas > 0 ? (double)sumaDesaprobadas / desaprobadas : 0;
+
+            Console.WriteLine($"Promedio de notas aprobadas: {promedioAprobadas}");
+            Console.WriteLine($"Promedio de notas desaprobadas: {promedioDesaprobadas}");
+            Console.WriteLine($"Cantidad de notas aprobadas: {aprobadas}");
+            Console.WriteLine($"Cantidad de notas desaprobadas: {desaprobadas}");
+        }
+    }
+}
+
+   
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using Ejercicios2_JSVE.Clases;
+using System;
+
+namespace Ejercicios2_JSVE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //int[] resultado = Clases.metodos.SumaDeArreglos();
+            //Console.WriteLine("Resultado de la suma:");
+            //for (int i = 0; i < resultado.Length; i++)
+            //{
+            //    Console.Write(resultado[i] + " ");
+            //}
+
+            //metodos metodos1 = new metodos();
+
+            //Console.WriteLine("Ingrese el tmañaño:");
+
+            //int tamañoA = int.Parse(Console.ReadLine());
+
+            //int[] arreglo = new int[tamañoA];
+
+            //for (int i = 0; i < tamañoA; i++)
+            //{
+            //    Console.Write($"Ingrese el valor {i + 1}: ");
+            //    arreglo[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Ingrese el elemento a buscar:");
+            //int elementoBuscado = int.Parse(Console.ReadLine());
+
+            //int[] coincidencia = metodos1.BuscarPosicionElemento(arreglo, elementoBuscado);
+
+            //Console.WriteLine("Posiciones de coincidencia:");
+            //foreach (int posicion in coincidencia)
+            //{
+            //    Console.WriteLine($"Posición: {posicion}");
+            //}
+
+
+
+            //metodos metodos1 = new metodos();
+
+            //int[] idsAlgebra = metodos1.ObtenerIDs("Álgebra");
+            //int[] idsAnalisis = metodos1.ObtenerIDs("Análisis");
+
+            //int[] alumnosComunes = metodos1.EncontrarAlumnosComunes(idsAlgebra, idsAnalisis);
+
+            //metodos1.MostrarAlumnos(idsAlgebra, "Alumnos inscritos en Álgebra:");
+            //metodos1.MostrarAlumnos(idsAnalisis, "Alumnos inscritos en Análisis:");
+            //metodos1.MostrarAlumnos(alumnosComunes, "Alumnos inscritos en ambas clases:");
+
+            //Console.WriteLine($"Número de alumnos que se repiten: {alumnosComunes.Length}");
+
+
+            //metodos metodos1 = new metodos();
+
+            //int[] arreglo = metodos1.LlenarArreglo();
+
+            //Console.WriteLine("Contenido del arreglo:");
+            //foreach (int elemento in arreglo)
+            //{
+            //    Console.WriteLine(elemento);
+            //}
+
+            //Console.ReadKey();
+
+            Console.WriteLine("Ingrese la cantidad de notas:");
+            int cantidadNotas = Convert.ToInt32(Console.ReadLine());
+            int[] notas = new int[cantidadNotas];
+
+            for (int i = 0; i < cantidadNotas; i++)
+            {
+                Console.Write($"Ingrese la nota {i + 1}: ");
+                notas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            metodos metodos1 = new metodos();
+            metodos1.CalcularPromedioYContar(notas);
+
+            Console.ReadKey();
+        }
+    }
+}
